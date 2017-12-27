4c1fe055e6bfa5f94ec0d67dcb65b1bc096325d4
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-27 20:53:33 +0100

    notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ui message" :class="type">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notification',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
