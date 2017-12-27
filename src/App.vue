ba635a9296bd559aaf0e9ad80c43b41ffeff7d89
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-27 18:51:01 +0100

    adding semantic UI

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: #f7f7f7;
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+
+  .is-danger {
+    color: #9f3a38;
+  }
 </style>
