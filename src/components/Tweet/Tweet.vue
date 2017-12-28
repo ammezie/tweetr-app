13d78b73d34de0735b30009752c260b7d0c21520
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-28 22:14:59 +0100

    tweet component

---
+++ src/components/Tweet/Tweet.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="event">
+    <div class="label">
+      <img src="https://www.gravatar.com/avatar/default?s=200&r=pg&d=mm">
+    </div>
+    <div class="content">
+      <div class="summary">
+        <router-link :to="`/${tweet.user.username}`">
+          {{ tweet.user.name }}
+          <small>{{ `@${tweet.user.username}` }}</small>
+        </router-link>
+        <div class="date">
+          {{ tweet.created_at | timeAgo }}
+        </div>
+      </div>
+      <router-link :to="`/${tweet.user.username}/status/${tweet.id}`">
+        <div class="extra text">
+          {{ tweet.tweet }}
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Tweet',
+    props: {
+      tweet: {
+        type: Object,
+        required: true
+      },
+      authUser: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import axios from 'axios'
+import moment from 'moment'
 import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 import App from './App'
@@ -16,6 +17,11 @@ axios.defaults.baseURL = 'http://127.0.0.1:3333'
 Vue.use(VeeValidate)
 
 Vue.config.productionTip = false
+
+// Global filter to format dates with momentjs
+Vue.filter('timeAgo', date => moment(date).fromNow())
+Vue.filter('joined', date => moment(date).format('MMMM YYYY'))
+Vue.filter('dob', date => moment(date).format('MMMM Do YYYY'))
 
 /* eslint-disable no-new */
 new Vue({
