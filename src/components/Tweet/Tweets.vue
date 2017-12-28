6effdcc01498f359090f30e31c78911ce44e39c2
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-28 22:15:16 +0100

    tweets component

---
+++ src/components/Tweet/Tweets.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ui feed">
+    <Tweet
+      v-for="tweet in tweets"
+      :key="tweet.id"
+      :tweet="tweet"
+    />
+  </div>
+</template>
+
+<script>
+  import Tweet from '@/components/Tweet/Tweet'
+
+  export default {
+    name: 'Tweets',
+    components: {
+      Tweet
+    },
+    props: {
+      tweets: {
+        type: Array,
+        required: true
+      },
+      authUser: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
