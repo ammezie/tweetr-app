535391dfb7ba788af099309ce4687de03613ae76
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-29 18:35:12 +0100

    replying a tweet

---
+++ src/components/Tweet/Replies.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="ui small feed">
+    <div class="event" v-for="reply in replies" :key="reply.id">
+      <div class="label">
+        <img src="https://www.gravatar.com/avatar/default?s=200&r=pg&d=mm">
+      </div>
+      <div class="content">
+        <div class="summary">
+          <router-link :to="reply.user.username">
+            {{ reply.user.name }}
+            <small>{{ `@${reply.user.username}` }}</small>
+          </router-link>
+          <div class="date">
+            {{ reply.created_at | timeAgo }}
+          </div>
+        </div>
+        <div class="extra text">
+          {{ reply.reply }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Replies',
+    props: {
+      replies: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
--- src/components/Tweet/SingleTweet.vue
+++ src/components/Tweet/SingleTweet.vue
@@ -21,16 +21,38 @@
         </div>
       </div>
       <div class="ui divider"></div>
+
+      <form class="ui form" @submit.prevent="replyTweet">
+        <div class="field">
+          <textarea name="reply" v-model="reply" rows="2" placeholder="Reply tweet"></textarea>
+        </div>
+
+        <button class="ui button primary" :disabled="!isFormValid">Reply</button>
+      </form>
+
+      <Replies :replies="replies"/>
     </div>
   </div>
 </template>
 
 <script>
+  import Replies from '@/components/Tweet/Replies';
+
   export default {
     name: 'SingleTweet',
+    components: {
+      Replies
+    },
     data () {
       return {
-        tweet: ''
+        tweet: '',
+        replies: [],
+        reply: ''
+      }
+    },
+    computed: {
+      isFormValid () {
+        return !!this.reply
       }
     },
     created () {
@@ -40,10 +62,30 @@
       fetchTweet () {
         axios.get(`/tweets/${this.$route.params.id}`).then(response => {
           this.tweet = response.data.data
+          this.replies = response.data.data.replies.reverse()
         })
       },
       back () {
         this.$router.go(-1)
+      },
+      replyTweet () {
+        const token = localStorage.getItem('tweetr-token')
+
+        axios
+          .post(
+            `/tweets/reply/${this.tweet.id}`,
+            { reply: this.reply },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            }
+          )
+          .then(response => {
+            this.reply = ''
+
+            this.replies.unshift(response.data.data)
+          })
       }
     }
   }
