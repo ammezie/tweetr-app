ca5101480de65aa4b619ff80c957b9236e2187bd
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-12-30 13:22:15 +0100

    displaying user followers

---
+++ src/components/User/Profile/UserFollowers.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <UserProfileHeader
+      :user="user"
+      :auth-user="authUser"
+    />
+    <div class="ui stackable grid container">
+      <div class="four wide column">
+        <UserProfileSidebarDetails :user="user"/>
+      </div>
+      <div class="twelve wide column">
+        <div class="ui segment">
+          <div class="ui three column grid">
+            <UserCard
+              v-for="follower in user.followers"
+              :key="follower.id"
+              :user="follower"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserProfileHeader from '@/components/User/Profile/UserProfileHeader'
+import UserProfileSidebarDetails from '@/components/User/Profile/UserProfileSidebarDetails'
+import UserCard from '@/components/User/Profile/UserCard'
+import profileMixin from '@/mixins/profileMixin'
+
+export default {
+  name: 'UserFollowers',
+  mixins: [profileMixin],
+  components: {
+    UserProfileHeader,
+    UserCard,
+    UserProfileSidebarDetails
+  },
+  data () {
+    return {
+      user: '',
+      authUser: ''
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Home from '@/components/Home'
 import SingleTweet from '@/components/Tweet/SingleTweet'
 import UserProfile from '@/components/User/Profile/UserProfile'
 import UsersFollowing from '@/components/User/Profile/UsersFollowing'
+import UserFollowers from '@/components/User/Profile/UserFollowers'
 
 Vue.use(Router)
 
@@ -41,6 +42,15 @@ export default new Router({
     {
       path: '/:username/following',
       component: UsersFollowing,
+      props: true
+    },
+    {
+      path: '/followers',
+      component: UserFollowers
+    },
+    {
+      path: '/:username/followers',
+      component: UserFollowers,
       props: true
     },
     {
